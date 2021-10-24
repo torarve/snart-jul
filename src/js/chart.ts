16b70ed8027f4fd7ea881fa0ea5dae663f9ff036
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-10-24 09:48:43 +0200

    Create plugin for showing current date when moving mouse over chart.

--- src/js/chart.ts
+++ src/js/chart.ts
@@ -1,8 +1,15 @@
 import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
+import { format } from 'date-fns';
 import { nb } from 'date-fns/locale';
 import 'chartjs-adapter-date-fns';
 
-import { firstSundayOfAdvent } from './functions';
+import { firstSundayOfAdvent, xmasFeeling } from './functions';
+
+function inside(point, area) {
+    return (area.top<point.y && point.y<area.bottom)
+        && (area.left<point.x && point.x<area.right);
+}
 
 export function createChart() {
     const year = new Date().getFullYear();
@@ -22,7 +29,53 @@ export function createChart() {
                     }
                 }
             },
+            events: ['mousemove', 'touchmove'],
+            onHover: function(evt, elements) {
+                const area = chart.chartArea;
+                const pos = getRelativePosition(evt, chart);
+                chart.$marker.inside = false;
+                if (inside(pos, area)) {
+                    chart.$marker.position = pos;
+                    chart.$marker.date = chart.scales.x.getValueForPixel(pos.x);
+                    chart.$marker.inside = true;
+                    chart.update();
+                }
+            },
         },
+        plugins: 
+        [
+            {
+                id: "my-chart-plugin",
+                beforeInit: function(chart) {
+                    chart.$marker = {
+                        position: {x: 0, y: 0}
+                    }
+                },
+                beforeDatasetsDraw: function(chart) {
+                    const marker = chart.$marker;
+                    const d = new Date(marker.date);
+                    if(marker.inside) {
+                        const area = chart.chartArea;
+                        const pos = marker.position;
+                        chart.ctx.save()
+                        chart.ctx.beginPath();
+                        chart.ctx.strokeStyle = "gray";
+                        chart.ctx.moveTo(pos.x, area.top);
+                        chart.ctx.lineTo(pos.x, area.bottom);
+                        chart.ctx.stroke();
+                        const dateStr = format(d, 'dd. MMM yyyy H:mm', { locale: nb });
+                        const feeling = xmasFeeling(d);
+                        const feelingStr = `Julefølelse ${(feeling*100).toFixed(2)}%`;
+                        const w = Math.max(chart.ctx.measureText(dateStr).width, chart.ctx.measureText(feelingStr).width);
+                        const xAdjust = (pos.x + w > area.right) ? -(w + 8) : 8;
+                        chart.ctx.fillStyle = "gray";
+                        chart.ctx.fillText(dateStr, pos.x + xAdjust, pos.y - 16);
+                        chart.ctx.fillText(feelingStr, pos.x + xAdjust, pos.y);
+                        chart.ctx.restore();
+                    }
+                },
+            }
+        ],
         data: {
             datasets: [{
                 label: `Julefølelse ${year}`,
